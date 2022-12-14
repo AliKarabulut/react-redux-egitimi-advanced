1cdbeba093ca3c72f00b2218dcec8bbf2f32c348
Ali Karabulut <a.kbt6666@gmail.com>
2022-12-14 21:26:51 +0300

    Proje ilk dosyalar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+      <p>asd</p>
+    </Layout>
+  );
+}
+
+export default App;
