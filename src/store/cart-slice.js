41ade9e92494a3749054236470663f9b6479dc5a
Ali Karabulut <a.kbt6666@gmail.com>
2022-12-18 20:54:12 +0300

    cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -17,13 +17,25 @@ createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
-        existingItem.quantity++
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItem() {},
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions
+export default cartSlice
